08d9fa62c45b619384a4bed4e77a8cefe285a50d
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-08-22 16:25:57 +0530

    Task Completed

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.error('Connection error', err);
+});
+
+
+const transactionRoutes = require("./routes/transactionRoutes");
+app.use("/", transactionRoutes);
+
+module.exports = app;
